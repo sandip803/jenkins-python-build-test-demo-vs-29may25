991ddc82b6b7a8dea0f979eacdf4e4a852ca8984
sandip803 <sandip93@gmail.com>
2025-05-29 10:48:46 +0530

    Test file first commit and push n 29 may 2025

---
+++ ops-test.py
@@ -0,0 +1,12 @@
+from ops import *
+def test_add():
+    assert add(2,3) == 5
+
+def test_subs(a,b):
+    assert subs(2,3) == -1
+
+def test_multi(a,b):
+    assert add(2,3) == 6
+
+def test_div(a,b):
+    assert div(10,5) == 2
